0185d7ea26022dc832602610f0dfd52854f9c71f
Ali Can Gönüllü <alicangonullu@yahoo.com>
2024-04-05 16:49:39 +0300

    SandAnalyze
    Sandbox Analyzer updated!

--- example.py
+++ example.py
@@ -1,8 +1,30 @@
+import sys, os
 from python_windows import *
 from python_linux import *
-# Run dllscollector.bat
-# Create a folder inside of examples/rootfs/windows_xx/bin
-# Copy malware inside of examples/rootfs/windows_xx/bin
-# Example EXE name : example.exe
-exeloc = input("Write to EXE name : ")
-QilingSandBox_Windows.runwindows(exeloc)
+print("""
+      # Run dllscollector.bat
+      Create a folder inside of examples/rootfs/windows_xx/bin
+      Copy malware inside of examples/rootfs/windows_xx/bin
+      Example EXE name : example.exe
+      """)
+
+filelist_x86 = os.listdir(os.getcwd() + "/examples/rootfs/x86_windows/bin")
+filelist_x8664 = os.listdir(os.getcwd() + "/examples/rootfs/x8664_windows/bin")
+print("=========================================")
+print("x86 File List")
+print("=========================================")
+for i in filelist_x86:
+    print("[*] " + i + "\n")
+print("=========================================")
+print("x64 File List")
+print("=========================================")
+for i in filelist_x8664:
+    print("[*] " + i + "\n")
+exeloc = str(sys.argv[1])
+if exeloc is None or exeloc == "":
+    print("Example example.py example.exe")
+else:
+    try:
+        QilingSandBox_Windows.runwindows(exeloc)
+    except:
+        print("Example example.py example.exe")
